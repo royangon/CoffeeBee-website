4d7ceb68399c0fb2e0b8e82411c38fb477dee8d9
Roy Angon <royangon29@gmail.com>
2024-10-18 23:41:27 +0600

    new commit

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ menuOpenButton.addEventListener("click", () => {
 });
 // close menu when the button is clicked //
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+// initialize swiper //
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows //
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   //   responsive breakpoints //
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    } 
+  });
